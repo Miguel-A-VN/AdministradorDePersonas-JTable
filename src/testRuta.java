8a1d228f8c4eb5908e56d0d2c47b612c66885604
Miguel-A-VN <mvargasnavarro19@gmail.com>
2025-08-11 17:14:30 -0500

    Agregando el proyecto de JPA

--- src/testRuta.java
+++
@@ -1,8 +0,0 @@
-import java.io.File;
-
-public class testRuta {
-    public static void main(String[] args) {
-        File file = new File("C:/Users/josey/OneDrive/Documentos/EjemploMVCPersona/src/properties/archivo.properties");
-        System.out.println("¿Existe el archivo?: " + file.exists());
-    }
-}
